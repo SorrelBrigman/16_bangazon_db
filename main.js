390f66f5c92126c1649b61cb98540f93486f09aa
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-09 12:52:19 -0600

    added auto increment and primary key to create tables

--- main.js
+++ main.js
@@ -12,26 +12,26 @@ const errorHandler = (err) => {
 };
 
 //create customers table
-db.run('CREATE TABLE IF NOT EXISTS customers(customerid INT, firstname TEXT, lastname TEXT, address TEXT, city TEXT, postalcode INT, phonenumber BLOB)', (err)=>{
+db.run('CREATE TABLE IF NOT EXISTS customers(customerid id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, address TEXT, city TEXT, postalcode INT, phonenumber BLOB)', (err)=>{
   errorHandler(err);
 });
 //create orders table
-db.run('CREATE TABLE IF NOT EXISTS orders(orderid INT, customerid INT, paymentid INT, paidinfull INT, date TEXT)', (err)=>{
+db.run('CREATE TABLE IF NOT EXISTS orders(orderid INTEGER PRIMARY KEY AUTOINCREMENT, customerid INT, paymentid INT, paidinfull INT, date TEXT)', (err)=>{
   errorHandler(err);
 });
 //create payment_options table
-db.run('CREATE TABLE IF NOT EXISTS payment_options(paymentid INT, name TEXT, accountnumber BLOB)', (err)=>{
+db.run('CREATE TABLE IF NOT EXISTS payment_options(paymentid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, accountnumber BLOB)', (err)=>{
   errorHandler(err);
 });
 //create products table
-db.run('CREATE TABLE IF NOT EXISTS products(productid INT, name TEXT, price NUMERIC(6,2), description BLOB)', (err)=>{
+db.run('CREATE TABLE IF NOT EXISTS products(productid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUMERIC(6,2), description BLOB)', (err)=>{
   errorHandler(err);
 });
 //create order_line_items table
-db.run('CREATE TABLE IF NOT EXISTS order_line_items(lineitemid INT, orderid INT, productid INT)', (err)=>{
+db.run('CREATE TABLE IF NOT EXISTS order_line_items(lineitemid INTEGER PRIMARY KEY AUTOINCREMENT, orderid INT, productid INT)', (err)=>{
   errorHandler(err);
 });
 //create product_review table
-db.run('CREATE TABLE IF NOT EXISTS product_review(reviewid INT, productid INT, customerid INT, rating INT, reviewtext BLOB)', (err)=>{
+db.run('CREATE TABLE IF NOT EXISTS product_review(reviewid INTEGER PRIMARY KEY AUTOINCREMENT, productid INT, customerid INT, rating INT, reviewtext BLOB)', (err)=>{
   errorHandler(err);
 });
